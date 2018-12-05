496d5698324b5ec4c75f5b4c6805efe5671a4739
Guillem Mateos <bbguillem@gmail.com>
2018-12-06 00:41:02 +0100

    Supress cURL output

--- deploy.sh
+++ deploy.sh
@@ -71,11 +71,11 @@ if [ $? -eq 0 ]; then
       git commit -m 'Update stack'
       if git push --set-upstream origin $STACK_ENV; then
         echo "State successfully saved to git. Marking deployment as succesful."
-        curl -d '{"state":"success","description":"Deployment finished successfully"}' -H 'Content-Type: application/json' -H "Authorization: Bearer $GIT_TOKEN" -sS "https://api.github.com/repos/$GIT_OWNER/$GIT_REPO/deployments/$DEPLOYMENT_ID/statuses"
+        curl -o /dev/null -d '{"state":"success","description":"Deployment finished successfully"}' -H 'Content-Type: application/json' -H "Authorization: Bearer $GIT_TOKEN" -sS "https://api.github.com/repos/$GIT_OWNER/$GIT_REPO/deployments/$DEPLOYMENT_ID/statuses"
       fi
     fi
   else
     echo "Deployment failed"
-    curl -d '{"state":"failure","description":"Deployment failed"}' -H 'Content-Type: application/json' -H "Authorization: Bearer $GIT_TOKEN" -sS "https://api.github.com/repos/$GIT_OWNER/$GIT_REPO/deployments/$DEPLOYMENT_ID/statuses"
+    curl -o /dev/null -d '{"state":"failure","description":"Deployment failed"}' -H 'Content-Type: application/json' -H "Authorization: Bearer $GIT_TOKEN" -sS "https://api.github.com/repos/$GIT_OWNER/$GIT_REPO/deployments/$DEPLOYMENT_ID/statuses"
   fi
 fi
