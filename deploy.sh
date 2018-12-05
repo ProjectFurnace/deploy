0c0d0994c39f38722df32f5846f5feba75421e70
Guillem Mateos <bbguillem@gmail.com>
2018-12-05 15:38:22 +0100

    Fixes to git side of things

--- deploy.sh
+++ deploy.sh
@@ -7,15 +7,22 @@ STATE_REPO="$(node /app/readyaml.js $TMP_DIR/stack.yaml state.repo)"
 STACK_NAME="$(node /app/readyaml.js $TMP_DIR/stack.yaml name)"
 STACK_REGION="$(node /app/readyaml.js $TMP_DIR/stack.yaml platform.aws.region)"
 
-STATE_REPO="${STATE_REPO/:\/\//://$GIT_TOKEN@}"
-
 echo "Git and Stack info"
 echo "STATE REPO: $STATE_REPO"
 echo "STACK NAME: $STACK_NAME"
+echo "STACK REGION: $STACK_REGION"
+
+cat $TMP_DIR/sources.yaml
+
+STATE_REPO="${STATE_REPO/:\/\//://$GIT_TOKEN@}"
+
+# initial git config
+git config --global user.email "hello@projectfurnace.io"
 
 # clone state repo to folder prev-config
 echo "Cloning state repo..."
-git clone $STATE_REPO prev-config
+rm -rf /tmp/pulumi-prev-config
+git clone $STATE_REPO /tmp/pulumi-prev-config
 # login to pulumi locally
 echo "Logging into pulumi locally..."
 pulumi login --local
@@ -31,7 +38,7 @@ if [ ! -f prev-config/config.checkpoint.json ]; then
 else
   # previous stack config found
   echo "Trying to import previous stack config..."
-  if pulumi stack import --file prev-config/config.checkpoint.json; then
+  if pulumi stack import --file /tmp/pulumi-prev-config/config.checkpoint.json; then
     echo "Selecting stack $STACK_NAME-$STACK_ENV..."
     pulumi stack select $STACK_NAME-$STACK_ENV
   fi
@@ -44,11 +51,20 @@ if [ $? -eq 0 ]; then
     echo "Stack successfully initiated! Saving pulumi checkpoint..."
     # export current stack state
     mkdir -p new-config
-    if pulumi stack export --file new-config/config.checkpoint.json; then
+    if pulumi stack export --file /tmp/pulumi-prev-config/config.checkpoint.json; then
       # push new state to github
       echo "Checkpoint succesfully saved. Commiting and pushing to github..."
-      ( cd new-config; git commit -m 'Update stack')
-      ( cd new-config; git push)
+      cd /tmp/pulumi-prev-config
+      # delete old origin so we can add the token
+      git remote rm origin
+      git remote add origin $STATE_REPO
+      # commit to github
+      git checkout -b $STACK_ENV
+      git add .
+      git commit -m 'Update stack'
+      if git push --set-upstream origin $STACK_ENV; then
+        echo "State successfully saved to git"
+      fi
     fi
   fi
 fi
