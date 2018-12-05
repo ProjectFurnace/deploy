dfae7b3dd850e4fb3d57016fc1bc440fe6b995df
Guillem Mateos <bbguillem@gmail.com>
2018-12-06 00:46:27 +0100

    Pending status for deployment

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ echo "GIT OWNER: $GIT_OWNER"
 echo "GIT REPO: $GIT_REPO"
 
 # set status as pending for deployment
-curl -o /dev/null -d '{"state":"pending","description":"Deployment started..."}' -H 'Content-Type: application/json' -H "Authorization: Bearer $GIT_TOKEN" -sS "https://api.github.com/repos/$GIT_OWNER/$GIT_REPO/deployments/$DEPLOYMENT_ID/statuses"
+curl -o /dev/null -d '{"state":"in_progress","description":"Deployment started..."}' -H 'Content-Type: application/json' -H 'Accept: application/vnd.github.flash-preview+json' -H "Authorization: Bearer $GIT_TOKEN" -sS "https://api.github.com/repos/$GIT_OWNER/$GIT_REPO/deployments/$DEPLOYMENT_ID/statuses"
 
 STATE_REPO="${STATE_REPO/:\/\//://$GIT_TOKEN@}"
 
